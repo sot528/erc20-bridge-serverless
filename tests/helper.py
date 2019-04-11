ca35dd87b6fccec15f3466783a657f1a2a407970
Go Hayakawa <go.hayakawa@gmail.com>
2019-04-11 17:20:47 +0900

    Modify tests/helper.py

--- tests/helper.py
+++ tests/helper.py
@@ -1,7 +1,12 @@
 import boto3
 
 dynamodb_client = boto3.resource(
-    'dynamodb', endpoint_url='http://localhost:8000/')
+    'dynamodb',
+    endpoint_url='http://localhost:8000/',
+    region_name='ap-northeast-1.',
+    aws_access_key_id='ACCESS_ID',
+    aws_secret_access_key='ACCESS_KEY'
+)
 
 
 def create_erc20_bridge_info_db_table():
